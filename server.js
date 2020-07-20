e74015a120f86ccf4c1f579d9af6dc28b574a0df
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-19 20:22:07 -0700

    Adding dependencies

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 // require(path.join(__dirname, "routes/apiRoutes.js"))(app);
 require(path.join(__dirname, "routes/htmlRoutes.js"))(app);
