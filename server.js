be5ff1353f4b363a12d3fcfe382c05060a96ee8f
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-19 21:32:05 -0700

    Just like the post method, 'Note' and 'Notes were intertwined within a delete function. Delete did not recognize anything to delete. FIXED

--- server.js
+++ server.js
@@ -39,15 +39,15 @@ app.delete("/api/notes/:id", function(req, res){
     var noteID = req.params.id;
     note = fs.readFileSync("./db/db.json");
     note = note.filter(function(notes){
-        if (noteID === notes.id){
+        if (noteID === note.id){
             return false;
         }
         else{
             return true;
         }
     })
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
-    res.json(notes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(note));
+    res.json(note);
 });
 
 
